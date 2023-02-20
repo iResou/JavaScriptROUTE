af04d5f5c0b777dfb229610e32df8339b3f5b169
Andrés Felipe Burbano Hernández <andrefelbu@hotmail.com>
2023-02-19 23:21:01 -0500

    first

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+//Burbuja
 var burbuja= document.getElementById("burbuja");
 document.addEventListener("mousemove",function(event){
 var x=event.clientX;
@@ -9,7 +10,35 @@ burbuja.animate({
 left: x+"px",
 top: y+"px"
 },{duration: 3000,fill: "forwards"});
-
-
-
 });
+
+//Random text
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 50);
+}
